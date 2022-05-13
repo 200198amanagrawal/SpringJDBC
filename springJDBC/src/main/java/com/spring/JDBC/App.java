90691d4b1093fd56f9c22692be5ef47c0db0c217
amanagrawal <amanagrawal2098@gmail.com>
2022-05-14 01:01:29 +0530

    multiple element query

--- springJDBC/src/main/java/com/spring/JDBC/App.java
+++ springJDBC/src/main/java/com/spring/JDBC/App.java
@@ -1,5 +1,7 @@
 package com.spring.JDBC;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,6 +22,14 @@ public class App
         String query="Select * from student where sid=?";
         //rowmapperimpl mapRow method returns an object of student type and that will be used as a single output
        Student student= template.queryForObject(query,new RowMapperImpl(),1);
+       
+       //for multiple use query
+       List<Student> list=template.query("select * from student", new RowMapperImpl());
+       for(Student s:list)
+       {
+    	   System.out.println(s);
+       }
+       System.out.println("******** Single ************");
        System.out.println(student);
     }
 }
